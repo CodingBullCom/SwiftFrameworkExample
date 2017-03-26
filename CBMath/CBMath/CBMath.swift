9271de8e3bf9d49a21ed9d2394aeca05595dc8d4
Pawan <pawan4444@gmail.com>
2017-03-27 01:43:58 +0530

    Initial Commit

---
+++ CBMath/CBMath/CBMath.swift
@@ -0,0 +1,32 @@
+//
+//  CBMath.swift
+//  CBMath
+//
+//  Created by Pawan Singh on 26/03/17.
+//  Copyright © 2017 Pawan Singh. All rights reserved.
+//
+
+import Foundation
+
+public class CBMath{
+    
+    public init(){
+        
+    }
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a+b
+    }
+    
+    public func subtract(a: Int, b: Int) -> Int {
+        return a-b
+    }
+    
+    public func multiply(a: Int, b: Int) -> Int {
+        return a*b
+    }
+    
+    public func divide(a: Int, b: Int) -> Int {
+        return a%b
+    }
+}
---
+++ MathTest/CBMath.framework/Headers/CBMath.h
@@ -0,0 +1,19 @@
+//
+//  CBMath.h
+//  CBMath
+//
+//  Created by Pawan Singh on 26/03/17.
+//  Copyright © 2017 Pawan Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CBMath.
+FOUNDATION_EXPORT double CBMathVersionNumber;
+
+//! Project version string for CBMath.
+FOUNDATION_EXPORT const unsigned char CBMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBMath/PublicHeader.h>
+
+
